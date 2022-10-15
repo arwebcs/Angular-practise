96df1e0b82a26fecaa4bdfcf7417d6fa698889f0
arweb20 <tutorcode992@gmail.com>
2022-10-15 20:38:05 +0530

    TD-forms complete

--- src/app/td-forms/td-forms.component.ts
+++ src/app/td-forms/td-forms.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-td-forms',
@@ -7,41 +7,57 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./td-forms.component.css'],
 })
 export class TdFormsComponent implements OnInit {
+  @ViewChild('frm') myForm: NgForm | undefined
   constructor() {}
-  
-  nameErr: string = '';
-  emailErr: string = '';
-  ageErr: string = '';
+
+  nameErr: string = ''
+  emailErr: string = ''
+  ageErr: string = ''
 
   ngOnInit(): void {}
-
-  onSubmit(frm: NgForm) {
-    console.log(frm);
-    if (frm.controls['nm'].errors) {
-      if(frm.controls['nm'].errors['required']){
-        this.nameErr = 'Please enter name';
-      }else{
-        this.nameErr = 'Please enter at least '+frm.controls['nm'].errors['minlength'].requiredLength
-        +' characters';
-      }
-    }else{
-      this.nameErr='';
-    }
-    if (frm.controls['em'].errors) {
-      if(frm.controls['em'].errors['required']){
-        this.emailErr = 'Please enter email';
-      }
-    }else{
-      this.emailErr='';
-    }
-    if (frm.controls['ag'].errors) {
-      if(frm.controls['ag'].errors['required']){
-        this.ageErr = 'Please enter age';
-      }else{
-        this.ageErr = 'Age cannot exceed '+frm.controls['ag'].errors['max'].max+' years';
-      }
-    }else{
-      this.ageErr='';
-    }
+  onPopulate() {
+    this.myForm?.form.patchValue({
+      nm: 'Arghya',
+      ag: 132,
+      em: 'a@gmail.com',
+    })
   }
+  onReset() {
+    this.myForm?.reset()
+  }
+  onSubmit() {
+    console.log(this.myForm)
+  }
+  // onSubmit(frm: NgForm) {
+  //   console.log(frm)
+  //   if (frm.controls['nm'].errors) {
+  //     if (frm.controls['nm'].errors['required']) {
+  //       this.nameErr = 'Please enter name'
+  //     } else {
+  //       this.nameErr =
+  //         'Please enter at least ' +
+  //         frm.controls['nm'].errors['minlength'].requiredLength +
+  //         ' characters'
+  //     }
+  //   } else {
+  //     this.nameErr = ''
+  //   }
+  //   if (frm.controls['em'].errors) {
+  //     if (frm.controls['em'].errors['required']) {
+  //       this.emailErr = 'Please enter email'
+  //     }
+  //   } else {
+  //     this.emailErr = ''
+  //   }
+  //   if (frm.controls['ag'].errors) {
+  //     if (frm.controls['ag'].errors['required']) {
+  //       this.ageErr = 'Please enter age'
+  //     } else {
+  //       this.ageErr =
+  //         'Age cannot exceed ' + frm.controls['ag'].errors['max'].max + ' years'
+  //     }
+  //   } else {
+  //     this.ageErr = ''
+  //   }
+  // }
 }
