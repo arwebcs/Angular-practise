bab47ad17f37332dc730341046284885af7be155
arweb20 <tutorcode992@gmail.com>
2022-10-21 23:08:03 +0530

    Reactive forms update

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -10,10 +10,13 @@ export class ReactiveFormsComponent implements OnInit {
   constructor() {}
 
   myForm: FormGroup | any
-
+  academic: FormGroup | any
   nameErr: string = ''
   emailErr: string = ''
   ageErr: string = ''
+  genderErr: string = ''
+  classErr: string = ''
+  secErr: string = ''
 
   ngOnInit(): void {
     this.myForm = new FormGroup({
@@ -23,6 +26,11 @@ export class ReactiveFormsComponent implements OnInit {
       ]),
       email: new FormControl('', [Validators.email, Validators.required]),
       age: new FormControl('', [Validators.required, Validators.max(100)]),
+      gender: new FormControl('', [Validators.required]),
+      academic: new FormGroup({
+        class: new FormControl('', [Validators.required]),
+        sec: new FormControl('', [Validators.required]),
+      }),
     })
   }
   onPopulate() {
@@ -30,6 +38,11 @@ export class ReactiveFormsComponent implements OnInit {
       sname: new FormControl('Soumyanjan'),
       email: new FormControl('arghya992@gmail.com'),
       age: new FormControl(76),
+      gender: new FormControl('Male'),
+      academic: new FormGroup({
+        class: new FormControl('Engg'),
+        sec: new FormControl('CSE'),
+      }),
     })
   }
   onReset() {
@@ -37,20 +50,20 @@ export class ReactiveFormsComponent implements OnInit {
   }
   onSubmit() {
     console.log(this.myForm)
-    if (this.myForm.controls['sname'].errors) {
-      if (this.myForm.controls['sname'].errors['required']) {
+    if (this.myForm.get('sname').errors) {
+      if (this.myForm.get('sname').errors['required']) {
         this.nameErr = 'Please enter name'
       } else {
         this.nameErr =
           'Please enter at least ' +
-          this.myForm.controls['sname'].errors['minlength'].requiredLength +
+          this.myForm.get('sname').errors['minlength'].requiredLength +
           ' characters'
       }
     } else {
       this.nameErr = ''
     }
-    if (this.myForm.controls['email'].errors) {
-      if (this.myForm.controls['email'].errors['required']) {
+    if (this.myForm.get('email').errors) {
+      if (this.myForm.get('email').errors['required']) {
         this.emailErr = 'Please enter email'
       } else {
         this.emailErr = 'Invalid email'
@@ -58,17 +71,41 @@ export class ReactiveFormsComponent implements OnInit {
     } else {
       this.emailErr = ''
     }
-    if (this.myForm.controls['age'].errors) {
-      if (this.myForm.controls['age'].errors['required']) {
+    if (this.myForm.get('age').errors) {
+      if (this.myForm.get('age').errors['required']) {
         this.ageErr = 'Please enter age'
       } else {
         this.ageErr =
           'Age cannot exceed ' +
-          this.myForm.controls['age'].errors['max'].max +
+          this.myForm.get('age').errors['max'].max +
           ' years'
       }
     } else {
       this.ageErr = ''
+    }
+    if (this.myForm.get('gender').errors) {
+      if (this.myForm.get('gender').errors['required']) {
+        this.genderErr = 'Please select gender'
+      }
+    } else {
+      this.genderErr = ''
+    }
+
+    if (this.myForm.controls['academic'].controls['class'].errors) {
+      if (
+        this.myForm.controls['academic'].controls['class'].errors['required']
+      ) {
+        this.classErr = 'Please select class'
+      }
+    } else {
+      this.classErr = ''
+    }
+    if (this.myForm.controls['academic'].controls['sec'].errors) {
+      if (this.myForm.controls['academic'].controls['sec'].errors['required']) {
+        this.secErr = 'Please select section'
+      }
+    } else {
+      this.secErr = ''
     }
   }
 }
